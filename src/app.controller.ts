be465d372aa0985e564850af35cc1d514f5ee5b7
Phaonex <phaonex@icloud.com>
2023-09-21 18:40:41 +0200

    [ADD] more end points: pokemon/limit, pokemon/pokedex/:id.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PockeApiService } from './services/pocke-api/pocke-api.service';
+import { string } from '@stdlib/stdlib';
 
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private readonly pockeService: PockeApiService) {}
 
-  @Get('.')
+  @Get(".")
   getHomeList(): any {
     return this.pockeService.catchThemAll({
-      query: `SELECT * FROM cloud-run-pokedex.pokemons.pokedex LIMIT 10`,
+      query: `SELECT * FROM cloud-run-pokedex.pokemons.pokedex LIMIT 100`,
       // Location must match that of the dataset(s) referenced in the query.
-      location: 'EU',
+      location: "EU",
     });
     
   }
 
-  @Get('pokemon/')
+  @Get("pokemon/")
   getPokemonsList(): any {
     return this.pockeService.catchThemAll({
-      query: `SELECT * FROM cloud-run-pokedex.pokemons.pokedex LIMIT 10`,
+      query: `SELECT * FROM cloud-run-pokedex.pokemons.pokedex`,
       // Location must match that of the dataset(s) referenced in the query.
-      location: 'EU',
+      location: "EU",
+    });
+    
+  }
+
+  @Get("pokemon/:limit")
+  getPokemonsListLimited(@Param('limit') limit: number): any {
+    console.log(limit)
+    return this.pockeService.catchThemAll({
+      query: `SELECT * FROM cloud-run-pokedex.pokemons.pokedex LIMIT ${limit}`,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: "EU",
+    });
+    
+  }
+
+  @Get("pokemon/pokedex/:id")
+  getPokemonsDetailList(@Param("id") id: string): any {
+    return this.pockeService.catchThemAll({
+      query: `SELECT * FROM cloud-run-pokedex.pokemons.detail_infomations WHERE pokedex_number = ${id}`,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: "EU",
     });
     
   }
